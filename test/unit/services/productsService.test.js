ef6eeeb6b86cdb69196a140d2d105c2cbf609ace
Ques-Mendes <quesmendes@gmail.com>
2022-06-06 20:27:40 -0300

    feat: add tests to service layer

--- test/unit/services/productsService.test.js
+++ test/unit/services/productsService.test.js
@@ -30,7 +30,7 @@ describe('Tests for productService layer', () => {
         productsModel.getById.restore();
       });
 
-      it('Return an object with error mssage "Product not found"', async () => {
+      it('Return an object with error message "Product not found"', async () => {
         const id = 1;
         const response = await productsService.getProductById(id);
         expect(response).to.be.an('object');
@@ -78,8 +78,8 @@ describe('Tests for productService layer', () => {
     });
   });
 
-    describe('Test fn update product', () => {
-    describe('When id is found', () => {
+  describe('Test fn update product', () => {
+    describe('When id is not found', () => {
 
       before(() => {
         sinon.stub(productsModel, 'updateProduct').resolves([[{}, {}], []])
@@ -92,6 +92,62 @@ describe('Tests for productService layer', () => {
         const received = { name: 'name', quantity: 10, id: 1 };
         const response = await productsService.updateProduct(received);
         expect(response).to.be.an('object');
+        expect(response.message).to.be.equal('Product not found');
+      });
+    });
+
+    describe('When id exists', () => {
+
+      before(() => {
+        sinon.stub(productsModel, 'updateProduct').resolves([{ affectedRows: 1 }]);
+      });
+      after(() => {
+        productsModel.updateProduct.restore();
+      });
+
+      it('Return an obj with the rigth data', async () => {
+        const received = { name: 'name', quantity: 10, id: 1 };
+        const response = await productsService.updateProduct(received);
+        expect(response).to.be.an('object');
+        expect(response).to.includes.all.keys('id', 'name', 'quantity');
+      })
+    })
+  });
+
+  describe('Test fn delete product', () => {
+    describe('When id is not found', () => {
+
+      before(() => {
+        sinon.stub(productsModel, 'deleteProduct').resolves([{ affectedRows: 0 }]);
+      });
+      after(() => {
+        productsModel.deleteProduct.restore();
+      });
+
+      it('Return an object with error message "Product not found"', async () => {
+        const id = 1;
+        const response = await productsService.deleteProduct(id);
+        expect(response).to.be.an('object');
+        expect(response).to.includes.all.keys('error', 'message');
+        expect(response.message).to.be.equal('Product not found');
+      });
+    })
+
+    describe('When id exists', () => {
+
+      before(() => {
+        sinon.stub(productsModel, 'deleteProduct').resolves([{ affectedRows: 1 }]);
+      });
+      after(() => {
+        productsModel.deleteProduct.restore();
+      });
+
+      it('Return an obj with id and delete message true', async () => {
+        const id = 1;
+        const response = await productsService.deleteProduct(id);
+        expect(response).to.be.an('object');
+        expect(response).to.includes.all.keys('id', 'deleted');
+        expect(response.id).to.be.equal(1);
       });
     });
   });
