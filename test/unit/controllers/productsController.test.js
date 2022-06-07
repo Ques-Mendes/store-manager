2606af8502b8d61f29c9cd2725d39321b89cd616
Ques-Mendes <quesmendes@gmail.com>
2022-06-07 14:18:24 -0300

    feat: add tests to product controller layer

--- test/unit/controllers/productsController.test.js
+++ test/unit/controllers/productsController.test.js
@@ -65,13 +65,11 @@ describe('Tests productController', () => {
         name: "Martelo de Thor",
         quantity: 10
       }; 
-
       sinon.stub(productsService, 'getProductById').resolves(serviceResponse);     
       response.status = sinon.stub().returns(response);
       response.json = sinon.stub().returns();
       request.params ={ id: 1 }; 
     });
-
     after(() => {
       productsService.getProductById.restore();
     });
@@ -82,6 +80,166 @@ describe('Tests productController', () => {
       expect(response.json.calledWith({ id: 1,
         name: "Martelo de Thor",
         quantity: 10 })).to.be.equal(true);
+    });
+  });
+
+  describe('Test fn create product when product exists', () => {
+
+    const request = {};
+    const response = {};
+    const serviceResponseError = {
+      error: true,
+      message: 'Product already exists',
+    };
+
+    before(() => {
+      sinon.stub(productsService, 'createProduct').resolves(serviceResponseError);
+      response.status = sinon.stub().returns(response);
+      response.json = sinon.stub().returns();
+      request.body = { "name": "produto", "quantity": 100 };
+    });
+    after(() => {
+      productsService.createProduct.restore();
+    });
+
+    it('Return an obj with error and status 409', async () => {
+      await productsController.createProduct(request, response);
+      expect(response.status.calledWith(409)).to.be.equal(true);
+      expect(response.json.calledWith({ "message": "Product already exists" })).to.be.equal(true);
+    });
+  });
+
+  describe('Test fn create product when register a new product', () => {
+
+    const request = {};
+    const response = {};
+    const serviceResponseCreated = {
+      "id": 1,
+      "name": "produto",
+      "quantity": 10
+    };
+
+    before(() => {
+      sinon.stub(productsService, 'createProduct').resolves(serviceResponseCreated);
+      response.status = sinon.stub().returns(response);
+      response.json = sinon.stub().returns();
+      request.body = { "name": "produto", "quantity": 100 };
+    });
+    after(() => {
+      productsService.createProduct.restore();
+    });
+
+    it('Return an obj and status 201', async () => {
+      await productsController.createProduct(request, response);
+      expect(response.status.calledWith(201)).to.be.equal(true);
+      expect(response.json.calledWith(serviceResponseCreated)).to.be.equal(true);
+    });
+  });
+
+  describe('Test fn update product when id is not found', () => {
+
+    const request = {};
+    const response = {};
+    const serviceResponseError = {
+      error: true,
+      message: 'Product not found',
+    };
+
+    before(() => {
+      sinon.stub(productsService, 'updateProduct').resolves(serviceResponseError);
+      response.status = sinon.stub().returns(response);
+      response.json = sinon.stub().returns();
+      request.params = { id: 1 };
+      request.body = {  "name": "produto", "quantity": 100 };
+    });
+    after(() => {
+      productsService.updateProduct.restore();
+    });
+
+    it('Return an obj with error and status 404', async () => {
+      await productsController.updateProduct(request, response);
+      expect(response.status.calledWith(404)).to.be.equal(true);
+      expect(response.json.calledWith({ "message": "Product not found" })).to.be.equal(true);
+    });
+  });
+
+  describe('Test fn update product when id is found', () => {
+
+    const request = {};
+    const response = {};
+    const serviceResponse = {
+      "id": 1,
+      "name": "produto",
+      "quantity": 10
+    };
+
+    before(() => {
+      sinon.stub(productsService, 'updateProduct').resolves(serviceResponse);
+      response.status = sinon.stub().returns(response);
+      response.json = sinon.stub().returns();
+      request.params = { id: 1 };
+      request.body = {  "name": "produto", "quantity": 100 };
+    });
+    after(() => {
+      productsService.updateProduct.restore();
+    });
+
+    it('Return an obj and status 409', async () => {
+      await productsController.updateProduct(request, response);
+      expect(response.status.calledWith(409)).to.be.equal(true);
+      expect(response.json.calledWith(serviceResponse)).to.be.equal(true);
+    });
+  });
+
+  describe('Test fn delete product when id is not found', () => {
+
+    const request = {};
+    const response = {};
+    const serviceResponseError = {
+      "error": true,
+      "message": "Product not found",
+    };
+
+    before(() => {
+      sinon.stub(productsService, 'deleteProduct').resolves(serviceResponseError);
+      response.status = sinon.stub().returns(response);
+      response.json = sinon.stub().returns();
+      request.params = { id: 1 };
+    });
+    after(() => {
+      productsService.deleteProduct.restore();
+    });
+
+    it('Return an obj with error and status 404', async () => {
+      await productsController.deleteProduct(request, response);
+      expect(response.status.calledWith(404)).to.be.equal(true);
+      expect(response.json.calledWith({ "message": "Product not found" })).to.be.equal(true);
+    });
+  });
+
+  describe('Test fn delete product when id is found', () => {
+
+    const request = {};
+    const response = {};
+    const serviceResponse = {
+      "id": 1,
+      "deleted": true
+    };
+
+    before(() => {
+      sinon.stub(productsService, 'deleteProduct').resolves(serviceResponse);
+      response.status = sinon.stub().returns(response);
+      response.json = sinon.stub().returns();
+      request.params = { id: 1 };
+    });
+    after(() => {
+      productsService.deleteProduct.restore();
+    });
+
+    it('Return status 204', async () => {
+      await productsController.deleteProduct(request, response);
+      expect(response.status.calledWith(204)).to.be.equal(true);
+      expect(response.end.calledOnce).to.be.equal(true);
     });
   });
 });
